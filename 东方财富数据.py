61a1d2dc065d868e2e2873c27fad4f7e07b5fdde
RunningMaoDou <65071754+LiuMaoDou@users.noreply.github.com>
2021-11-16 06:13:21 +0800

    Update 东方财富数据.py

--- 东方财富数据.py
+++ 东方财富数据.py
@@ -7,11 +7,11 @@ import pymongo
 import time
 
 mongo_client = pymongo.MongoClient('127.0.0.1', 27017)
-
+mongo_db = mongo_client['股票']
 # today = datetime.date.today().strftime('%Y-%m-%d')
 
 # today = datetime.datetime.today()
-today = datetime.datetime(year=2021, month=11, day=13, hour=0, minute=0, second=0)
+today = datetime.datetime(year=2021, month=11, day=15, hour=0, minute=0, second=0)
 
 
 def timer(function):
@@ -31,9 +31,8 @@ def timer(function):
     return wrapper
 
 
-
+@timer
 def stock_index():
-    mongo_db = mongo_client['股票']
     mongo_collection = mongo_db['指数']
     pattern = re.compile(r'.*?\((.*?)\).*')
     lst = []
@@ -69,7 +68,6 @@ def stock_index():
 
 @timer
 def daily_data():
-    mongo_db = mongo_client['股票']
     mongo_collection = mongo_db['每日股票']
 
     pattern = re.compile(r'.*?\((.*?)\).*')
@@ -119,7 +117,7 @@ def daily_data():
         if i['f6'] == "-":
             cj = 0
         else:
-            cj = float(i['f6'])/100000000
+            cj = float(i['f6']) / 100000000
 
         dic = {'名称': i['f14'], '日期': today, '最新价': i['f2'], '涨跌幅': i['f3'], '成交额(亿)': cj,
                'PE': i['f9']}
@@ -129,6 +127,62 @@ def daily_data():
     print('------Finished------')
 
 
+@timer
+def group_data():
+    mongo_collection = mongo_db['板块']
+
+    pattern = re.compile(r'.*?\((.*?)\).*')
+    lst = []
+
+    data = {
+        'cb': 'jQuery1123039820439462558643_1637012892751',
+        'fid': 'f62',
+        'po': '1',
+        'pz': '50',
+        'pn': '1',
+        'np': '1',
+        'fltt': '2',
+        'invt': '2',
+        'ut': 'b2884a393a59ad64002292a3e90d46a5',
+        'fs': 'm:90 t:2',
+        'fields': 'f14'
+    }
+
+    result = requests.get("http://50.push2.eastmoney.com/api/qt/clist/get", params=data)
+
+    newR = pattern.sub("\g<1>", result.text)
+    newR2 = json.loads(newR)
+
+    data = {
+        'cb': 'jQuery1123039820439462558643_1637012892751',
+        'fid': 'f62',
+        'po': '1',
+        'pz': newR2['data']['total'],
+        'pn': '1',
+        'np': '1',
+        'fltt': '2',
+        'invt': '2',
+        'ut': 'b2884a393a59ad64002292a3e90d46a5',
+        'fs': 'm:90 t:2',
+        'fields': 'f3,f14,f62,f204'
+    }
+
+    result = requests.get("http://push2.eastmoney.com/api/qt/clist/get", params=data)
+
+    newR = pattern.sub("\g<1>", result.text)
+    newR2 = json.loads(newR)
+
+    for i in newR2['data']['diff']:
+        cj = round((float(i['f62']) / 100000000), 2)
+
+        dic = {'名称': i['f14'], '日期': today, '涨跌幅': i['f3'], '净额(亿)': cj, '关注股票': i['f204']}
+        lst.append(dic)
+
+    mongo_collection.insert_many(lst)
+    print('------Finished------')
+
+
 if __name__ == "__main__":
     # stock_index()
-    daily_data()
+    # daily_data()
+    group_data()

5dd39fe47c2ec01b66ff542286590ceb85f284dd
RunningMaoDou <65071754+LiuMaoDou@users.noreply.github.com>
2022-01-01 19:55:49 +0800

    Update 批量改名.py

--- 批量改名.py
+++ 批量改名.py
@@ -2,16 +2,22 @@ import shutil, os
 from pathlib import Path
 import re
 
-filepath = "/Users/**/Downloads/rrshare/绝望的主妇第1季"
+filepath = "/Users/**/Music/轻音乐/"
 p = Path(filepath)
 
-# 绝望的主妇.Desperate.Housewives.S01E02.Chi_Eng.HR-HDTV.AAC.1024X576.x264-YYeTs人人影视
 
-files = p.glob('*')
-pattern = re.compile('(Desperate.Housewives.S\d+E\d+)')
+# files = p.glob('長笛琴人*')
+files = p.glob('*《*?》*')
+
+pattern = re.compile('.*?(《.*?》).*')
 for i in files:
     old = i.name
-    new = pattern.search(old)[1] + '.mp4'
-    shutil.move(p / old, p / new)
-    
+    if "長笛琴人" not in str(i):
+        print(pattern.search(old)[1])
+
+        new = pattern.search(old)[1] + '.mp3'
+        shutil.move(p / old, p / new)
+    # print(pattern.search(old)[1])
+    # print(old)
+
 print("Done")
